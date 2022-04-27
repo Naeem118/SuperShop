37b2c6d101285f2ac6e28bd80948c67abb9895c4
naeem118 <gmnaeem7@gmail.com>
2022-04-28 01:57:52 +0600

    Important File Configured

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
+from django.db import connection
 from django.shortcuts import render
-
+from django.db import connection
+from supershop import definitions
 # Create your views here.
 def home(request):
-    return render(request,'pages/home.html')
+    cursor=connection.cursor()
+    query="""select CATEGORY_NAME from product_category"""
+    cursor.execute(query)
+    result=definitions.dictfetchall(cursor)
+    ans=[ x['CATEGORY_NAME'] for x in result ]
+    data={'categories':ans}
+    return render(request,'pages/home.html',data)
